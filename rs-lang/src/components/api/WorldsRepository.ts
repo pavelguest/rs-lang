bb9d87bc28541ed697796056936b2c9c81d7151b
Pavel <pavelguest@yandex.ru>
2022-02-02 22:35:49 +0300

    RSL-02

---
+++ rs-lang/src/components/api/WorldsRepository.ts
@@ -0,0 +1,19 @@
+class WorldsRepository {
+  baseUrl: string = 'https://rsslang.herokuapp.com';
+  words: string = `${this.baseUrl}/words`;
+
+  async all(page: number = 0, group: number = 0) {
+    const res = await (
+      await fetch(`${this.words}?page=${page}&group=${group}`)
+    ).json();
+    console.log(res);
+    return res;
+  }
+  async get(id: number) {
+    const res = await (await fetch(`${this.words}/${id}`)).json();
+    console.log(res);
+    return res;
+  }
+}
+
+const worldsRepository = new WorldsRepository();
